3f4a7ecf8d2d9252b389e1101ae7ce14462efb34
marinehayong <marinehayong@gmail.com>
2019-12-17 16:19:24 +0900

    스크래피 처음 사용

---
+++ ecommerce/ecommerce/spiders/gmarket.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class GmarketSpider(scrapy.Spider):
+    name = 'gmarket'
+    allowed_domains = ['http://www.gmarket.co.kr/']
+    start_urls = ['http://http://www.gmarket.co.kr//']
+
+    def parse(self, response):
+        print(response.text)
