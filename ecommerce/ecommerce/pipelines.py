c562055afe6981ae762c2c050577cdb8c3b95874
marinehayong <marinehayong@gmail.com>
2019-12-18 16:05:31 +0900

    강력/최신 크롤링 기술: Scrapy pipeline 사용법 익히기 2

--- ecommerce/ecommerce/pipelines.py
+++ ecommerce/ecommerce/pipelines.py
@@ -4,8 +4,12 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+from scrapy.exceptions import DropItem
 
 class EcommercePipeline(object):
     def process_item(self, item, spider):
-        return item
+        print('process_item', item)
+        if int(item['price'])>10000:
+            return item
+        else:
+            raise DropItem("dropitem",item)
