a609132bfb5340f801b1b223abf8af08b8dce791
marinehayong <marinehayong@gmail.com>
2019-12-18 14:11:37 +0900

    강력/최신 크롤링 기술: Scrapy로 지마켓 크롤링 하기 1

--- ecommerce/ecommerce/spiders/gmarket.py
+++ ecommerce/ecommerce/spiders/gmarket.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from ecommerce.items import EcommerceItem
 
 
 class GmarketSpider(scrapy.Spider):
     name = 'gmarket'
-    allowed_domains = ['http://www.gmarket.co.kr/']
-    start_urls = ['http://http://www.gmarket.co.kr//']
+    allowed_domains = ['corners.gmarket.co.kr/Bestsellers']
+    start_urls = ['http://corners.gmarket.co.kr/Bestsellers/']
 
     def parse(self, response):
-        print(response.text)
+        titles = response.css('div.best-list li > a::text').getall()
+        for title in titles:
+            doc = EcommerceItem()
+            doc['title'] = title
+            yield doc
