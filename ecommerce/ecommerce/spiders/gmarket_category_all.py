25c901cfc83c529fd9f67979c1f171285783392f
marinehayong <marinehayong@gmail.com>
2019-12-19 10:57:32 +0900

    실전 크롤링: 지마켓 크롤링하며 scrapy 실전 활용법 익히기 3

--- ecommerce/ecommerce/spiders/gmarket_category_all.py
+++ ecommerce/ecommerce/spiders/gmarket_category_all.py
@@ -12,4 +12,11 @@ class GmarketCategoryAllSpider(scrapy.Spider):
         category_links = response.css('div.gbest-cate ul.by-group li a::attr(href)').getall()
         category_names = response.css('div.gbest-cate ul.by-group li a::text').getall()
         for index, category_link in enumerate(category_links):
-            print('http://corners.gmarket.co.kr' + category_link, category_names[index])
+            # category_names[index]
+            yield scrapy.Request(url='http://corners.gmarket.co.kr' + category_link, callback=self.parse_maincategory, meta={'maincategory_name':category_names[index]})
+
+    def parse_maincategory(self, response):
+        print("parse_maincategory", response.meta['maincategory_name'])
+
+        best_items = response.css('div.best-list')
+        best_items.css('li')
